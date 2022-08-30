5d4caf900ed47050475f6f6659471213acac2fea
Christopher Samson <Chrissamson2000@gmail.com>
2022-08-30 16:39:17 +0800

    Added ExpensesFilter conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,19 +11,32 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent =  <p>No Expenses Found!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem 
+        key={expense.id}
+        title={expense.title} 
+        amount={expense.amount} 
+        date={expense.date} 
+      />
+    ));
+  };
+
   return (
     <div>
       <Card className='expenses'>
          {/* THis is considered a controlled component */}
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem 
-            key={expense.id}
-            title={expense.title} 
-            amount={expense.amount} 
-            date={expense.date} 
-          />
-        ))}
+        <ExpensesFilter 
+          selected={filteredYear} 
+          onChangeFilter={filterChangeHandler} 
+        />
+        {expensesContent}
     </Card>
     </div>
     
